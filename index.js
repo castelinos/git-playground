e20a8e397351daab597f1863385cd9614b8539a4
castelinos <sheltcas10@gmail.com>
2024-01-19 20:23:13 +0530

    Setup eslint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const a = 100;
 const b = 50;
 
-function getNumbers( num1, num2 ){
+function getNumbers(num1, num2) {
     return `${num1} and ${num2} are given set of numbers`;
 }
 
-console.log( "Hello User,",getNumbers( a, b ) );
+console.log( "Hello User," , getNumbers(a, b) );
